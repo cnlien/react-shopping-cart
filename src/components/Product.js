11b9c10091fe7f573dc9a0ddb8564dc7e2400479
Christopher Lien <cnlien@gmail.com>
2019-11-14 18:34:37 -0500

    MVP

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,3 +17,24 @@ const Product = props => {
 };
 
 export default Product;
+
+
+// import React from 'react';
+
+// const Product =()=> {
+// 	return (
+// 		<div className="product">
+// 			<img src={product.image} alt={`${product.title} book`} />
+
+// 			<h1 className="title">{product.title}</h1>
+
+// 			<p className="price">${product.price}</p>
+
+// 			<button onClick={() => addItem(product)}>
+// 				Add to cart
+// 			</button>
+// 		</div>
+// 	);
+// };
+
+// export default Product;
